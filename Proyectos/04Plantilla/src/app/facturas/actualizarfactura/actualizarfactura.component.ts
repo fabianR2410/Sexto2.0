05b123a8002d155d8f0988c91abce0488ede7102
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-28 21:48:59 -0500

    borrar facturas

---
+++ Proyectos/04Plantilla/src/app/facturas/actualizarfactura/actualizarfactura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizarfactura',
+  standalone: true,
+  imports: [],
+  templateUrl: './actualizarfactura.component.html',
+  styleUrl: './actualizarfactura.component.scss'
+})
+export class ActualizarfacturaComponent {
+
+}
