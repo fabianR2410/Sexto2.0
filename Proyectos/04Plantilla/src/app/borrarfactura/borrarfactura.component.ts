c82c4714e46092c627635eaaee229d618fe62e53
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-29 22:17:23 -0500

    bf

--- Proyectos/04Plantilla/src/app/borrarfactura/borrarfactura.component.ts
+++ Proyectos/04Plantilla/src/app/borrarfactura/borrarfactura.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+/*/
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { IFactura } from '../Interfaces/factura';
+import { Router, RouterLink } from '@angular/router';
+import { FacturaService } from '../Services/factura.service';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-borrarfactura',
+  selector: 'app-facturas',
   standalone: true,
-  imports: [],
-  templateUrl: './borrarfactura.component.html',
-  styleUrl: './borrarfactura.component.scss'
+  imports: [SharedModule, RouterLink],
+  templateUrl: './facturas.component.html',
+  styleUrl: './facturas.component.scss'
 })
-export class BorrarfacturaComponent {
+export class FacturasComponent implements OnInit {
+  listafacturas: IFactura[] = [];
+  constructor(private facturaServicio: FacturaService) {}
+  ngOnInit(): void {
+    this.facturaServicio.todos().subscribe((data: IFactura[]) => {
+      this.listafacturas = data;
+    });
+  }
 
+  eliminar(idFactura) {
+
+    Swal.fire({
+      title: 'Factura',
+      text: 'Esta seguro que desea eliminar la factura!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Emliminar Factura'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.facturaServicio.eliminar(idFactura).subscribe((data) => {
+          Swal.fire('Factura', 'La factura ha sido eliminada.', 'success');
+        this.ngOnInit();
+        });
+      }
+    });
+  }
 }
--- Proyectos/04Plantilla/src/app/facturas/facturas.component.ts
+++ Proyectos/04Plantilla/src/app/facturas/facturas.component.ts
@@ -3,6 +3,7 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { IFactura } from '../Interfaces/factura';
 import { Router, RouterLink } from '@angular/router';
 import { FacturaService } from '../Services/factura.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-facturas',
@@ -20,5 +21,23 @@ export class FacturasComponent implements OnInit {
     });
   }
 
-  eliminar(idFactura) {}
+  eliminar(idFactura) {
+
+    Swal.fire({
+      title: 'Factura',
+      text: 'Esta seguro que desea eliminar la factura!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Emliminar Factura'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.facturaServicio.eliminar(idFactura).subscribe((data) => {
+          Swal.fire('Factura', 'La factura ha sido eliminada.', 'success');
+        this.ngOnInit();
+        });
+      }
+    });
+  }
 }
